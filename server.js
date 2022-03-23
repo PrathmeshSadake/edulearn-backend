16d6168cd8ee8f3873600333eea5d913db0c8d5f
Prathmesh Sadake <prathmeshsadake@gmail.com>
2022-03-23 20:14:49 +0530

    Installed Cors

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express');
+var cors = require('cors');
 const fetch = (...args) =>
   import('node-fetch').then(({ default: fetch }) => fetch(...args));
 require('dotenv').config();
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 const API_KEY = process.env.WHEREBY_API_KEY;
